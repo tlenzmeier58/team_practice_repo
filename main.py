a36df6ee0d93df47ffbfb9888e9ffa99167f9c1d
Thomas Lenzmeier <thomas.lenzmeier@constellationmutual.com>
2022-07-28 19:52:17 -0500

    Dom's directions, update main.py

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 # This is the main.py for branching and merging
-a = 100
+# Tom is adding a new comment here, Monty what's behind door number one?
+a = 99
+my_var = "What's up is down, what's down is up"
